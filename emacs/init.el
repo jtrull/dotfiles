85f9a67470d6ee08213601087b43a00c933f7e7d
Jonathan Trull <1119601+jtrull@users.noreply.github.com>
2021-03-02 13:02:23 -0600

    Explicitly enable global diff modes

--- emacs/init.el
+++ emacs/init.el
@@ -192,8 +192,8 @@
 (use-package diff-hl
   :ensure t
   :config
-  (global-diff-hl-mode)
-  (diff-hl-flydiff-mode))
+  (global-diff-hl-mode 1)
+  (diff-hl-flydiff-mode 1))
 
 (use-package treemacs
   :ensure t
