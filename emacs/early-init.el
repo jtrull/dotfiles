6bcbe2a0a6458b2c3f1c05b2f675f9521593f594
Jonathan Trull <1119601+jtrull@users.noreply.github.com>
2021-04-13 09:50:18 -0500

    Fix lint warnings

--- emacs/early-init.el
+++ emacs/early-init.el
@@ -1,3 +1,13 @@
+;;; early-init.el --- Jonathan's Emacs configuration
+
+;;; Commentary:
+
+;; This is my personal Emacs configuration.  You probably don't want any.
+
+;;; Code:
+
 (require 'package)
 (add-to-list 'package-archives
 	     '("melpa-stable" . "https://stable.melpa.org/packages/"))
+
+;;; early-init.el ends here
