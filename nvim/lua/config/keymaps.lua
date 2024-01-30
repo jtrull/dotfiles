d8c6784e5f4ef00c77d03ac41d0a724a2938e1af
Jonathan Trull <1119601+jtrull@users.noreply.github.com>
2024-01-30 17:03:25 -0600

    Add :nohl shortcut

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -8,3 +8,6 @@ vim.keymap.set('n', '<leader>dd', vim.diagnostic.open_float)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_next)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_prev)
 vim.keymap.set('n', '<leader>dq', vim.diagnostic.setloclist)
+
+vim.keymap.set('n', '<leader>\\', '<cmd>nohl<cr>')
+
