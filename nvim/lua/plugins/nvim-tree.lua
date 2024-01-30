3d3d6ec315d67a6dfcaeefe9358b1e10c196d514
Jonathan Trull <1119601+jtrull@users.noreply.github.com>
2024-01-30 17:04:40 -0600

    Focus current file in nvim-tree

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -6,6 +6,11 @@ return {
     vim.g.loaded_netrw = 1
     vim.g.loaded_netrwPlugin = 1
   end,
+  opts = {
+    update_focused_file = {
+      enable = true
+    }
+  },
   config = true,
   keys = {
     { "<leader>e", "<cmd>NvimTreeFocus<cr>", desc = "Focus file explorer" }
