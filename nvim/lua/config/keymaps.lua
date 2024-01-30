7bc609f80961ae222cf1f94292f900bacc3c3e32
Jonathan Trull <1119601+jtrull@users.noreply.github.com>
2024-01-30 17:03:11 -0600

    Add LSP keymappings

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,3 +2,9 @@ vim.keymap.set({ 'n', 'i', 'v', 'o' }, '<Up>', '<Nop>')
 vim.keymap.set({ 'n', 'i', 'v', 'o' }, '<Down>', '<Nop>')
 vim.keymap.set({ 'n', 'i', 'v', 'o' }, '<Left>', '<Nop>')
 vim.keymap.set({ 'n', 'i', 'v', 'o' }, '<Right>', '<Nop>')
+
+-- Diagnostics
+vim.keymap.set('n', '<leader>dd', vim.diagnostic.open_float)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_next)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', '<leader>dq', vim.diagnostic.setloclist)
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -73,6 +73,25 @@ return {
       lspconfig.tsserver.setup { capabilities = cmp_caps }
       lspconfig.terraformls.setup { capabilities = cmp_caps }
       lspconfig.yamlls.setup { capabilities = cmp_caps }
+
+      vim.api.nvim_create_autocmd('LspAttach', {
+        group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+        callback = function(ev)
+          -- Buffer local mappings
+          local opts = { buffer = ev.buf }
+          vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+          vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+          vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+          vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+          vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+          vim.keymap.set('n', '<localleader>lk', vim.lsp.buf.signature_help, opts)
+          vim.keymap.set('n', '<localleader>lr', vim.lsp.buf.rename, opts)
+          vim.keymap.set('n', '<localleader>la', vim.lsp.buf.code_action, opts)
+          vim.keymap.set('n', '<localleader>lf', function()
+            vim.lsp.buf.format { async = true }
+          end, opts)
+        end
+      })
     end
   }
 }
