2f09d9d9ab70764af315e3db47fa341a69483ce8
Jonathan Trull <1119601+jtrull@users.noreply.github.com>
2024-04-24 16:01:51 -0500

    Revert "Don't complete Copilot if nothing has been typed"
    This reverts commit 5bc38a72678c89840b432f85c0bfb4c4c3f05812.

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -1,9 +1,3 @@
-local has_words_before = function()
-  if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_text(0, line-1, 0, line-1, col, {})[1]:match("^%s*$") == nil
-end
-
 return {
   "hrsh7th/nvim-cmp",
   version = false,
@@ -26,13 +20,7 @@ return {
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.abort(),
-        ['<TAB>'] = vim.schedule_wrap(function (fallback)
-          if cmp.visible() and has_words_before() then
-            cmp.mapping.confirm({ select = true })
-          else
-            fallback()
-          end
-        end)
+        ['<TAB>'] = cmp.mapping.confirm({ select = true })
       }),
       sources = cmp.config.sources({
         { name = "copilot" },
