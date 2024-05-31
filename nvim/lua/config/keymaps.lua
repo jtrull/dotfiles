38e473a3bbd2c7f66635c915cfb530e9ce8f321d
Jonathan Trull <1119601+jtrull@users.noreply.github.com>
2024-05-31 14:33:28 -0500

    Update 0.10 remove diagnostic mapping
    This mapping is built in now.

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -5,8 +5,6 @@ vim.keymap.set({ 'n', 'i', 'v', 'o' }, '<Right>', '<Nop>')
 
 -- Diagnostics
 vim.keymap.set('n', '<leader>dd', vim.diagnostic.open_float)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_next)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_prev)
 vim.keymap.set('n', '<leader>dq', vim.diagnostic.setloclist)
 
 vim.keymap.set('n', '<leader>\\', '<cmd>nohl<cr>')
