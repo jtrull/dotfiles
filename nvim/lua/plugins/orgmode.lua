d5b3970897a1cdb50f7adbc882c74fb01e0a6b2a
Jonathan Trull <1119601+jtrull@users.noreply.github.com>
2024-06-25 11:24:48 -0500

    Remove org-mode dependency on treesitter

--- nvim/lua/plugins/orgmode.lua
+++ nvim/lua/plugins/orgmode.lua
@@ -1,21 +1,8 @@
 return {
   {
     'nvim-orgmode/orgmode',
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter'
-    },
     ft = 'org',
     config = function()
-      require('orgmode').setup_ts_grammar()
-
-      require('nvim-treesitter.configs').setup({
-        highlight = {
-          enable = true,
-          additional_vim_regex_highlighting = { 'org' }
-        },
-        ensure_installed = { 'org' }
-      })
-
       require('orgmode').setup({
         org_agenda_files = '~/org/**/*.org',
         org_default_notes_file = '~/org/todo.org',
