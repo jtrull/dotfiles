09796c3103611a9e85763baa28a76c5b72260c20
Jonathan Trull <jonathan.trull@wearemci.com>
2025-02-07 17:17:10 -0600

    Only select completion if words are already typed
    This prevents accidentally selecting a Copilot completion when the
    intent is to use TAB to indent a line.

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -1,3 +1,9 @@
+local has_words_before = function()
+  if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_text(0, line-1, 0, line-1, col, {})[1]:match("^%s*$") == nil
+end
+
 return {
   "hrsh7th/nvim-cmp",
   version = false,
@@ -19,7 +25,13 @@ return {
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.abort(),
-        ['<TAB>'] = cmp.mapping.confirm({ select = true })
+        ["<Tab>"] = vim.schedule_wrap(function(fallback)
+          if cmp.visible() and has_words_before() then
+            cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+          else
+            fallback()
+          end
+        end),
       }),
       sources = cmp.config.sources({
         { name = "copilot" },
