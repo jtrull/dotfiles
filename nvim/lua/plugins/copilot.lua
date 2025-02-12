25f583a76bfc3badec64f75b9de04dea6a271fea
Jonathan Trull <jonathan.trull@wearemci.com>
2025-02-12 15:41:44 -0600

    Add Github Copilot Chat

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -16,5 +16,25 @@ return {
     "zbirenbaum/copilot-cmp",
     dependencies = { "zbirenbaum/copilot.lua" },
     config = true
+  },
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
+      "zbirenbaum/copilot.lua",
+      "nvim-lua/plenary.nvim"
+    },
+    config = true,
+    keys = {
+      { "<leader>cc", "<cmd>CopilotChatToggle<cr>", desc = "CopilotChat: Toggle window" },
+      { "<leader>ck", "<cmd>CopilotChatStop<cr>", desc = "Copilot Chat: Stop output" },
+      { "<leader>cq", "<cmd>CopilotChatReset<cr>", desc = "Copilot Chat: Reset" },
+      { "<leader>cx", "<cmd>CopilotChatExplain<cr>", desc = "Copilot Chat: Explain" },
+      { "<leader>cr", "<cmd>CopilotChatReview<cr>", desc = "Copilot Chat: Review" },
+      { "<leader>cf", "<cmd>CopilotChatFix<cr>", desc = "Copilot Chat: Fix" },
+      { "<leader>co", "<cmd>CopilotChatOptimize<cr>", desc = "Copilot Chat: Optimize" },
+      { "<leader>cd", "<cmd>CopilotChatDocs<cr>", desc = "Copilot Chat: Generate documentation comments" },
+      { "<leader>ct", "<cmd>CopilotChatTests<cr>", desc = "Copilot Chat: Generate tests" },
+      { "<leader>cg", "<cmd>CopilotChatCommit<cr>", desc = "Copilot Chat: Generate commit message" }
+    }
   }
 }
