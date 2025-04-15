6142ca72287a3ccf50e49a27f352a1645d6baf5d
Jonathan Trull <jonathan.trull@wearemci.com>
2025-04-15 10:45:41 -0500

    Update LSP mappings for new defaults in nvim 0.11

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -7,5 +7,9 @@ vim.keymap.set({ 'n', 'i', 'v', 'o' }, '<Right>', '<Nop>')
 vim.keymap.set('n', '<leader>dd', vim.diagnostic.open_float)
 vim.keymap.set('n', '<leader>dq', vim.diagnostic.setloclist)
 
+-- Additional LSP mappings
+vim.keymap.set('n', 'grD', vim.lsp.buf.declaration)
+vim.keymap.set('n', 'grd', vim.lsp.buf.definition)
+
 vim.keymap.set('n', '<leader>\\', '<cmd>nohl<cr>')
 
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -34,25 +34,6 @@ return {
       lspconfig.terraformls.setup { capabilities = capabilities }
       lspconfig.yamlls.setup { capabilities = capabilities }
       lspconfig.eslint.setup { capabilities = capabilities }
-
-      vim.api.nvim_create_autocmd('LspAttach', {
-        group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-        callback = function(ev)
-          -- Buffer local mappings
-          local opts = { buffer = ev.buf }
-          vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-          vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-          vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-          vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-          vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-          vim.keymap.set('n', '<localleader>lk', vim.lsp.buf.signature_help, opts)
-          vim.keymap.set('n', '<localleader>lr', vim.lsp.buf.rename, opts)
-          vim.keymap.set('n', '<localleader>la', vim.lsp.buf.code_action, opts)
-          vim.keymap.set('n', '<localleader>lf', function()
-            vim.lsp.buf.format { async = true }
-          end, opts)
-        end
-      })
     end
   }
 }
