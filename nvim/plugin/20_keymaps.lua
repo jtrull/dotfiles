c1fdd5761253a0598f4c14572c104a21b8e88020
Jonathan Trull <1119601+jtrull@users.noreply.github.com>
2026-04-02 09:06:40 -0500

    Move by wrapped line

--- nvim/plugin/20_keymaps.lua
+++ nvim/plugin/20_keymaps.lua
@@ -3,6 +3,10 @@ vim.keymap.set({ 'n', 'i', 'v', 'o' }, '<Down>', '<Nop>')
 vim.keymap.set({ 'n', 'i', 'v', 'o' }, '<Left>', '<Nop>')
 vim.keymap.set({ 'n', 'i', 'v', 'o' }, '<Right>', '<Nop>')
 
+-- Movement
+vim.keymap.set({ 'n', 'v', 's', 'o' }, 'j', "v:count ? 'j' : 'gj'", { expr = true, silent = true })
+vim.keymap.set({ 'n', 'v', 's', 'o' }, 'k', "v:count ? 'k' : 'gk'", { expr = true, silent = true })
+
 -- Diagnostics
 vim.keymap.set('n', '<leader>dd', vim.diagnostic.open_float)
 vim.keymap.set('n', '<leader>dq', vim.diagnostic.setloclist)
@@ -12,5 +16,4 @@ vim.keymap.set('n', 'grD', vim.lsp.buf.declaration)
 vim.keymap.set('n', 'grd', vim.lsp.buf.definition)
 
 vim.keymap.set('n', '<leader>\\', '<cmd>nohl<cr>')
-
 
