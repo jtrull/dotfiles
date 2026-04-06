0befde597d0a38cb3b19faa28f71fcaef52a59ce
Jonathan Trull <1119601+jtrull@users.noreply.github.com>
2026-04-06 17:28:48 -0500

    Copilot LSP config
    Disable telemetry, only attach to coding filetypes.

---
+++ nvim/after/lsp/copilot.lua
@@ -0,0 +1,33 @@
+return {
+  settings = {
+    telemetry = { enable = false }
+  },
+  filetypes = {
+    bash = true,
+    css = true,
+    dockerfile = true,
+    graphql = true,
+    html = true,
+    java = true,
+    javascript = true,
+    json = true,
+    lua = true,
+    make = true,
+    perl = true,
+    prisma = true,
+    python = true,
+    ruby = true,
+    scss = true,
+    sh = true,
+    sql = true,
+    terraform = true,
+    typescript = true,
+    typescriptreact = true,
+    javascriptreact = true,
+    vim = true,
+    xml = true,
+    yaml = true,
+    zsh = true,
+    ["*"] = false,
+  }
+}
